ffeb7b7e404930e64f426c6a0da722e1093ee74d
Kaio <shamuskaio@gmail.com>
2017-05-25 04:49:00 -0300

    Learn

---
+++ myapp/app.js
@@ -0,0 +1,58 @@
+var express = require('express');
+var path = require('path');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+
+var expressNunjucks = require('express-nunjucks');
+
+// Sessão
+// Instalar: npm install express-session --save
+var session = require('express-session');
+
+var index = require('./routes/index');
+var users = require('./routes/users');
+var login = require('./routes/login');
+
+var app = express();
+
+// view engine setup. Passa uma lista contendo os diretórios com as views.
+app.set('views', [path.join(__dirname, 'views'), path.join(__dirname, 'views/login')]);
+var njk = expressNunjucks(app, {
+    autoescape: true,
+    express: app
+});
+
+// uncomment after placing your favicon in /public
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(session({secret: "malu", resave: false, saveUninitialized: true}));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', index);
+app.use('/users', users);
+app.use('/login', login);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
